923f0dd9580677b388e1fa533dedae30cbbd14be
shw779 <shw779@naver.com>
2025-02-14 19:36:20 +0900

    [MYH-4][#2][Feat]appIcon & splash screen 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyLevelUpApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyLevelUpApp extends StatelessWidget {
+  const MyLevelUpApp({super.key});
   @override
   Widget build(BuildContext context) {
+    // whenever your initialization is completed, remove the splash screen:
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FlutterNativeSplash.remove();
+    });
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
