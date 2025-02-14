6271672c70ab70272c8634259c746c357141de09
shw779 <shw779@naver.com>
2025-02-14 15:19:46 +0900

    [MYH-4][#2][Feat] 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
